1654a9adde35257a847ec4d6afe5952d85542c27
cherrycharan980 <45454892+cherrycharan980@users.noreply.github.com>
2018-12-13 15:46:17 +0530

    Add files via upload

---
+++ GetTitle.java
@@ -0,0 +1,22 @@
+package com.k2js.seliniumbasics.openurl;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class GetTitle {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver","F:\\Selinium training by jithendra sir\\Downloads\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver;
+		
+		driver  = new ChromeDriver();
+		
+		driver.get("https://www.facebook.com/");
+		driver.getTitle();
+		driver.get("http://newtours.demoaut.com/");
+		driver.getTitle();
+		
+
+	}
+
+}
